cf535b9123e902ff9d8fdd4924bb7e89b66fa089
my-freeedom <260360445@qq.com>
2018-03-25 20:34:29 +0800

    gengxin

--- Public/Wap/js/address/edit_address.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.delete').click(function(){
-		$(this).parent('li').remove();
-		console.log($('ul li:first'));
-		$('ul li i:first').addClass('current');
-	})
-})
--- Public/Wap/js/user/top_up_recharge.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.money ul li').click(function(e){
-		// console.log($(this).attr("data-sum"))
-		$(this).addClass('active').siblings().removeClass();
-		$('footer p span i').html($(this).attr("data-sum"));
-	})
-})
