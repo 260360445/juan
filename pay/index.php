4cb66d2c712b54b2667f0146a2104cf08b245c66
my-freedom <260360445@qq.com>
2018-04-02 17:10:02 +0800

    201842

--- pay/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	@date_default_timezone_set('PRC');
-	echo strtotime('20180307000000');
-	echo '<br>';
-	echo date('Y-m-d H:i:s',1520438400);
-	echo '<br>';
-	echo strtotime('20180307000000');
-	echo '<br>';
-	echo date('Y-m-d H:i:s',1514736000);
