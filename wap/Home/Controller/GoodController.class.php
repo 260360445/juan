99413c6c19d7f9506bd47b1b7a7f9f7b36e82644
my-freedom <260360445@qq.com>
2018-04-08 21:01:59 +0800

    201848

--- wap/Home/Controller/GoodController.class.php
+++ wap/Home/Controller/GoodController.class.php
@@ -10,20 +10,10 @@ class GoodController extends ComController {
         $id=I('get.id','');
         //商品详情
         $info = M('goods')
-            ->field('id,goods_title,goods_logo,price,sid,money,xfb,goods_small_title,kucun,cont,sell,comment')
+            ->field('id,title,price,glogo,gurl,store,sell,yhj_num,yhj_url,yhj_price,state,ddq_kai')
             ->where(['id'=>$id,'sta'=>3])
             ->find();
-        //评价
-        $comment = M('comment')
-            ->field('o.id,o.cont,o.ptime,u.nick_name,u.photo')
-            ->alias('o')
-            ->join('left join __USER_INFOR__ u on o.uid = u.uid')
-            ->where(['o.gid'=>$id])
-            ->limit('6')
-            ->order('o.id desc')
-            ->select();
-        $this->assign('comment',$comment);
-        if($info){
+        if($info['state']== '2'){
             $this->assign("type",'yes');
             $this->assign("info",$info);
         }
@@ -113,21 +103,59 @@ class GoodController extends ComController {
         $this->display();
     }
 
-    //特卖会场
+    //咚咚会场
     public function specialsale(){
-        //特卖商品
-        $total = M('goods')->where('tm = 2')->count();
-        $result = M('goods')
-            ->field('id,goods_title,goods_logo,price,xfb')
-            ->where(['tm'=>2,'sta'=>3])
-            ->order('tm_time desc')
-            ->limit('6')
+      //特卖最热商品
+        $result_hot = M('goods')
+            ->field('id,title,glogo,price,sell,yhj_num,yhj_sy_num')
+            ->where(['zd_ddq'=>2,'state'=>2])
+            ->order('zd_ddq_time desc')
+            ->limit(6)
             ->select();
-        $this->assign('page',$html);
+        //咚咚
+        $where='';
+        $time=date("H",time());
+        $starttime=date("H",mktime(9));
+        if($time < 9 || $time >= 9 && $time < 12){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>9];
+        }else if($time >= 12 && $time < 15){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>12];
+        }else if($time >= 15 && $time < 18){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>15];   
+        }else if($time >= 18 && $time < 21){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>18];
+        }else if($time >= 21 && $time < 24){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>21];
+        }else if($time >= 24 && $time < $starttime){
+            $where = ['ddq'=>2,'state'=>2,'ddq_kai'=>9];
+        }
+        $result = M('goods')
+            ->field('id,title,price,glogo,sell,yhj_num,yhj_sy_num,yhj_price,ddq_kai')
+            ->where($where)
+            ->limit('6')
+            ->order('ddq_time desc,id desc')
+            ->select();
         $this->assign('result',$result);
+        $this->assign('result_hot',$result_hot);
         $this->display();
     }
-    
+    //人气
+    public function popularity(){
+        $id=I('get.gclass','');
+        $where=['state'=>2] ;
+        if($id != ''){
+            $where['goods_cate_id']= $id;
+        }
+        //商品详情
+        $result = M('goods')
+            ->field('id,title,price,glogo,gurl,store,sell,yhj_num,yhj_sy_num,yhj_url,yhj_price,state')
+            ->where($where)
+            ->limit(6)
+            ->order('sell desc,time desc')
+            ->select();
+        $this->assign('info',$result);
+        $this->display();
+    }
     //新品上架
     public function newgood(){
         $result = M('goods')
--- wap/Home/Controller/IndexController.class.php
+++ wap/Home/Controller/IndexController.class.php
@@ -6,13 +6,9 @@ class IndexController extends ComController {
     	//轮播
         $ban = M('ban')->field("url,logo")->where(['state'=>2,'type'=>3])->order('sort desc,id desc')->select();
         $this->assign('ban',$ban);
-        $goods = M('goods');
-        //查找特卖商品
-        $good_tm = $goods->field("id,goods_title,goods_logo,price,money,xfb")->where(['tm'=>2,'sta'=>3])->order('tm_time desc')->limit('4')->select();
-        $this->assign('good_tm',$good_tm);
         //猜你喜欢商品
-        $rel = $goods->field("id,goods_title,goods_logo,price,money,xfb,sell")->where(['sta'=>3])->order('sell desc')->limit('40')->select();
-        $this->assign('goods',$rel);
+        $good = M('goods')->where(['state'=>2])->order('sell desc,time desc')->limit('50')->select();
+        $this->assign('goods',$good);
     	$this->display();
     }
 }
